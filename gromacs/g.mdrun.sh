000ee13e369efc8d3c4600d7a2cad38e31673aff
Youssef Eldakar <youssefeldakar@gmail.com>
2021-05-19 18:01:59 +0200

    gromacs - add GPU example

--- gromacs/g.mdrun.sh
+++ gromacs/g.mdrun.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-#SBATCH --job-name=mdrun
-#SBATCH --partition=cpu
-#SBATCH --ntasks=24
-#SBATCH --cpus-per-task=1
-#SBATCH --time=3-00:00:00
+#SBATCH --job-name=Apo
+#SBATCH --partition=gpu
+#SBATCH --account=g.my_acct
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:2
+#SBATCH --time=120:00:00
 
 # This is an example for running `gmx mdrun`, the "main computational
 # chemistry engine within GROMACS."
 #
-# This version of the example uses CPU resources via MPI.
+# This version of the example uses GPU resources.
 #
 # To run this example, an input file in the .tpr file format is
-# required, which is assumed to have been uploaded as `my_run.tpr` and placed
+# required, which is assumed to have been uploaded as `em.tpr` and placed
 # next to this script file.  See the following for information on the .tpr file
 # format:
 #
@@ -34,8 +35,8 @@
 #
 # https://manual.gromacs.org/current/onlinehelp/gmx-grompp.html
 #
-## This example is part of the BA-HPC examples package:
+# This example is part of the BA-HPC examples package:
 #
 # https://hpc.bibalex.org/examples/
 
-mpirun -np 24 gmx_mpi mdrun -s my_run.tpr -deffnm my_run
+gmx mdrun -nt 16 -s my_run.tpr -deffnm my_run
